ef2310ffa37112dac2fb2b74fcf472c13957f4ad
Apirak Panatkool <apirakb@gmail.com>
2022-02-18 14:24:22 +0700

    fix change background js

--- src/js/sushi.js
+++ src/js/sushi.js
@@ -1,6 +1,5 @@
-var changeBackground = function() {
-  console.log("in change background");
-  var ssContainer = document.getElementsByClassName('ss-container')[0];
-  ssContainer.style.backgroundImage = "url('https://source.unsplash.com/1440x1440?sig=" + Math.random();
+function changeBackground() {
+  var siteBody = document.querySelector(".ss-site .body");
+  siteBody.style.backgroundImage =
+    "url('https://source.unsplash.com/1440x1440?sig=" + Math.random();
 }
-

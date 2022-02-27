e48b08f5ed3144475c2cb6cf31db915c09203f79
Apirak Panatkool <apirakb@gmail.com>
2022-02-28 00:06:31 +0700

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# abort on errors
+set -e
+
+# change branch
+git checkout gh-pages
+
+# build
+npm install
+npm run build
+
+# git
+git add -A
+git commit -m "deploy"
+
+git pull git@github.com:sushiui/sushi.git main
+git push -f git@github.com:sushiui/sushi.git main:gh-pages
+
+git checkout main
